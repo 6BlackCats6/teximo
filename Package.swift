2caa1c0dcc5e9907da81cf9101ceb2a6057a55da
6BlackCats6 <alexseredauk@gmail.com>
2026-07-11 14:58:45 +0100

    Configure hotkey regression tests

--- Package.swift
+++ Package.swift
@@ -5,14 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "teximo",
+    platforms: [
+        .macOS(.v13)
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "teximo",
+            path: "Sources/Teximo",
+            exclude: ["AppIcon.png", "Info.plist"],
             swiftSettings: [
                 .swiftLanguageVersion(.v5)
             ]
+        ),
+        .testTarget(
+            name: "TeximoTests",
+            dependencies: ["teximo"]
         ),
     ]
 )
